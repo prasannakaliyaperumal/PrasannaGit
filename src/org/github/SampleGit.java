d236891343626bc6ce87e0fe05325963d52020db
prasannakaliyaperumal <heyviratprasanna@gmail.com>
2021-07-26 11:30:24 +0530

    The Love

--- src/org/github/SampleGit.java
+++ src/org/github/SampleGit.java
@@ -2,13 +2,13 @@ package org.github;
 
 public class SampleGit {
 	
-	private void method1() {
-		System.out.println("Sachin");
+	private void method3() {
+		System.out.println("Tendulkar");
 
 	}
 	
-	private void method2() {
-		System.out.println("Virat");
+	private void method4() {
+		System.out.println("Kohli");
 	}
 	
 	public static void main(String[] args) {
