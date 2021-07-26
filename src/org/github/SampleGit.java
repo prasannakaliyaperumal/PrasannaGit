f06ab93a0758fdd41642b8ff344fd55e69020340
prasannakaliyaperumal <heyviratprasanna@gmail.com>
2021-07-26 11:42:45 +0530

    Yuvan

--- src/org/github/SampleGit.java
+++ src/org/github/SampleGit.java
@@ -3,11 +3,13 @@ package org.github;
 public class SampleGit {
 	
 	private void method1() {
+		System.out.println("STR");
 		System.out.println("Sachin");
 
 	}
 	
 	private void method2() {
+		System.out.println("Dhoni");
 		System.out.println("Virat");
 	}
 	
