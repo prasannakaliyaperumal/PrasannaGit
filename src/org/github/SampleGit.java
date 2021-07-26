beada7fa6772f2b83a947197bb6ae6ecf31fb48f
prasannakaliyaperumal <heyviratprasanna@gmail.com>
2021-07-26 11:20:19 +0530

    4th Time

---
+++ src/org/github/SampleGit.java
@@ -0,0 +1,18 @@
+package org.github;
+
+public class SampleGit {
+	
+	private void method1() {
+		System.out.println("Sachin");
+
+	}
+	
+	private void method2() {
+		System.out.println("Virat");
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
